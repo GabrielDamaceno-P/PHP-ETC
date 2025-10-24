aea617c01496b880c9a108c23facaf0818a9fdde
GabrielDamaceno-P <gdamaceno2002@gmail.com>
2025-10-24 19:30:01 -0300

    php

--- usuario.php
+++ usuario.php
@@ -9,33 +9,5 @@ class Usuario {
  public  $email;
  public  $telefone;
  private $senhaHash;
-
-  public function _construct($conn,$nome,$endereco,$telefone,$email,$senha){
-    $this->conn =$conn;
-    $this->nome = trim($nome);
-    $this->endereco = $endereco;
-    $this->telefone = $telefone;
-
-    $email = strolower(trim($email));
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL));{
-         throw new Exception("E-email inválido");
-    }
-    $this->email = ($email);
-    this->senhaHash = password_has($senha, PASSWORD_DEFAULT);   
- }
- public function getid(){
-    return $this->usuario_id;
- }
- public function salvar(){
-    $sql = "INSERT INTO usuarios (nome, endereco, telefone, email,senha) VALUES (?,?,?,?,?)"
-    $stmt = $this->conn-> prepare($sql);
-    if ($stmt->execute([$this->nome, $this->endereco, $this->telefone, $this->email, $this->senhaHash])){
-        $this->usuario_id = $this->conn->lastInsertId();
-        return $this->usuario_id;
-    } else {
-        throw new Exception ("Erro ao cadastrar usuario.");
-    }
- }
 }
-
 ?>
